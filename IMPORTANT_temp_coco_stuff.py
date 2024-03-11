a364a9ac58654c8ccc90e8a884ba8c442470e707
turacan <kerim.turacan@outlook.com>
2024-03-11 13:58:55 +0100

    REAMDE.md is being edited

---
+++ IMPORTANT_temp_coco_stuff.py
@@ -0,0 +1,304 @@
+# this file is used to adjust the detectron2 COCOEvaluator, as it is necesarry that augmentations are used, which needs to be implemented 
+# in file keypoint_trainer.py called by function build_evaluator of class CustomTrainer(DefaultTrainer) 
+
+
+class COCOEvaluator(DatasetEvaluator):
+    """
+    Evaluate AR for object proposals, AP for instance detection/segmentation, AP
+    for keypoint detection outputs using COCO's metrics.
+    See http://cocodataset.org/#detection-eval and
+    http://cocodataset.org/#keypoints-eval to understand its metrics.
+    The metrics range from 0 to 100 (instead of 0 to 1), where a -1 or NaN means
+    the metric cannot be computed (e.g. due to no predictions made).
+
+    In addition to COCO, this evaluator is able to support any bounding box detection,
+    instance segmentation, or keypoint detection dataset.
+    """
+
+    def __init__(
+        self,
+        dataset_name,
+        tasks=None,
+        distributed=True,
+        output_dir=None,
+        cfg=None,
+        *,
+        max_dets_per_image=None,
+        use_fast_impl=True,
+        kpt_oks_sigmas=()
+    ):
+        """
+        Args:
+            dataset_name (str): name of the dataset to be evaluated.
+                It must have either the following corresponding metadata:
+
+                    "json_file": the path to the COCO format annotation
+
+                Or it must be in detectron2's standard dataset format
+                so it can be converted to COCO format automatically.
+            tasks (tuple[str]): tasks that can be evaluated under the given
+                configuration. A task is one of "bbox", "segm", "keypoints".
+                By default, will infer this automatically from predictions.
+            distributed (True): if True, will collect results from all ranks and run evaluation
+                in the main process.
+                Otherwise, will only evaluate the results in the current process.
+            output_dir (str): optional, an output directory to dump all
+                results predicted on the dataset. The dump contains two files:
+
+                1. "instances_predictions.pth" a file that can be loaded with `torch.load` and
+                   contains all the results in the format they are produced by the model.
+                2. "coco_instances_results.json" a json file in COCO's result format.
+            max_dets_per_image (int): limit on the maximum number of detections per image.
+                By default in COCO, this limit is to 100, but this can be customized
+                to be greater, as is needed in evaluation metrics AP fixed and AP pool
+                (see https://arxiv.org/pdf/2102.01066.pdf)
+                This doesn't affect keypoint evaluation.
+            use_fast_impl (bool): use a fast but **unofficial** implementation to compute AP.
+                Although the results should be very close to the official implementation in COCO
+                API, it is still recommended to compute results with the official API for use in
+                papers. The faster implementation also uses more RAM.
+            kpt_oks_sigmas (list[float]): The sigmas used to calculate keypoint OKS.
+                See http://cocodataset.org/#keypoints-eval
+                When empty, it will use the defaults in COCO.
+                Otherwise it should be the same length as ROI_KEYPOINT_HEAD.NUM_KEYPOINTS.
+        """
+        self._cfg = cfg
+        self._logger = logging.getLogger(__name__)
+        self._distributed = distributed
+        self._output_dir = output_dir
+        self._use_fast_impl = use_fast_impl
+
+        # COCOeval requires the limit on the number of detections per image (maxDets) to be a list
+        # with at least 3 elements. The default maxDets in COCOeval is [1, 10, 100], in which the
+        # 3rd element (100) is used as the limit on the number of detections per image when
+        # evaluating AP. COCOEvaluator expects an integer for max_dets_per_image, so for COCOeval,
+        # we reformat max_dets_per_image into [1, 10, max_dets_per_image], based on the defaults.
+        if max_dets_per_image is None:
+            max_dets_per_image = [1, 10, 100]
+        else:
+            max_dets_per_image = [1, 10, max_dets_per_image]
+        self._max_dets_per_image = max_dets_per_image
+
+        if tasks is not None and isinstance(tasks, CfgNode):
+            kpt_oks_sigmas = (
+                tasks.TEST.KEYPOINT_OKS_SIGMAS if not kpt_oks_sigmas else kpt_oks_sigmas
+            )
+            self._logger.warn(
+                "COCO Evaluator instantiated using config, this is deprecated behavior."
+                " Please pass in explicit arguments instead."
+            )
+            self._tasks = None  # Infering it from predictions should be better
+        else:
+            self._tasks = tasks
+
+        self._cpu_device = torch.device("cpu")
+
+        self._metadata = MetadataCatalog.get(dataset_name)
+        if not hasattr(self._metadata, "json_file"):
+            if output_dir is None:
+                raise ValueError(
+                    "output_dir must be provided to COCOEvaluator "
+                    "for datasets not in COCO format."
+                )
+            self._logger.info(f"Trying to convert '{dataset_name}' to COCO format ...")
+
+            cache_path = os.path.join(output_dir, f"{dataset_name}_coco_format.json")
+            self._metadata.json_file = cache_path
+            convert_to_coco_json(dataset_name, cache_path, cfg=self._cfg)
+
+        json_file = PathManager.get_local_path(self._metadata.json_file)
+        with contextlib.redirect_stdout(io.StringIO()):
+            self._coco_api = COCO(json_file)
+
+        # Test set json files do not contain annotations (evaluation must be
+        # performed using the COCO evaluation server).
+        self._do_evaluation = "annotations" in self._coco_api.dataset
+        if self._do_evaluation:
+            self._kpt_oks_sigmas = kpt_oks_sigmas
+            
+
+def convert_to_coco_json(dataset_name, output_file, allow_cached=True, cfg=None):
+    """
+    Converts dataset into COCO format and saves it to a json file.
+    dataset_name must be registered in DatasetCatalog and in detectron2's standard format.
+
+    Args:
+        dataset_name:
+            reference from the config file to the catalogs
+            must be registered in DatasetCatalog and in detectron2's standard format
+        output_file: path of json file that will be saved to
+        allow_cached: if json file is already present then skip conversion
+    """
+
+    # TODO: The dataset or the conversion script *may* change,
+    # a checksum would be useful for validating the cached data
+
+    PathManager.mkdirs(os.path.dirname(output_file))
+    with file_lock(output_file):
+        if PathManager.exists(output_file) and allow_cached:
+            logger.warning(
+                f"Using previously cached COCO format annotations at '{output_file}'. "
+                "You need to clear the cache file if your dataset has been modified."
+            )
+        else:
+            logger.info(f"Converting annotations of dataset '{dataset_name}' to COCO format ...)")
+            coco_dict = convert_to_coco_dict(dataset_name, cfg=cfg)
+
+            logger.info(f"Caching COCO format annotations at '{output_file}' ...")
+            tmp_file = output_file + ".tmp"
+            with PathManager.open(tmp_file, "w") as f:
+                json.dump(coco_dict, f)
+            shutil.move(tmp_file, output_file)
+
+
+
+def convert_to_coco_dict(dataset_name, cfg=None):
+    """
+    Convert an instance detection/segmentation or keypoint detection dataset
+    in detectron2's standard format into COCO json format.
+
+    Generic dataset description can be found here:
+    https://detectron2.readthedocs.io/tutorials/datasets.html#register-a-dataset
+
+    COCO data format description can be found here:
+    http://cocodataset.org/#format-data
+
+    Args:
+        dataset_name (str):
+            name of the source dataset
+            Must be registered in DatastCatalog and in detectron2's standard format.
+            Must have corresponding metadata "thing_classes"
+    Returns:
+        coco_dict: serializable dict in COCO json format
+    """
+
+    dataset_dicts = DatasetCatalog.get(dataset_name)
+    metadata = MetadataCatalog.get(dataset_name)
+
+    # unmap the category mapping ids for COCO
+    if hasattr(metadata, "thing_dataset_id_to_contiguous_id"):
+        reverse_id_mapping = {v: k for k, v in metadata.thing_dataset_id_to_contiguous_id.items()}
+        reverse_id_mapper = lambda contiguous_id: reverse_id_mapping[contiguous_id]  # noqa
+    else:
+        reverse_id_mapper = lambda contiguous_id: contiguous_id  # noqa
+
+    categories = [
+        {"id": reverse_id_mapper(id), "name": name}
+        for id, name in enumerate(metadata.thing_classes)
+    ]
+
+    logger.info("Converting dataset dicts into COCO format")
+    coco_images = []
+    coco_annotations = []
+
+    from custom_mapper import custom_test_dataset_transform
+    for image_id, image_dict in enumerate(dataset_dicts):
+        image_dict = custom_test_dataset_transform(image_dict, cfg) # convert to json serializable
+
+        coco_image = {
+            "id": image_dict.get("image_id", image_id),
+            "width": int(image_dict["width"]),
+            "height": int(image_dict["height"]),
+            "file_name": str(image_dict["file_name"]),
+        }
+        coco_images.append(coco_image)
+
+        anns_per_image = image_dict.get("annotations", [])
+        for annotation in anns_per_image:
+            # create a new dict with only COCO fields
+            coco_annotation = {}
+
+            # COCO requirement: XYWH box format for axis-align and XYWHA for rotated
+            bbox = annotation["bbox"]
+            if isinstance(bbox, np.ndarray):
+                if bbox.ndim != 1:
+                    raise ValueError(f"bbox has to be 1-dimensional. Got shape={bbox.shape}.")
+                bbox = bbox.tolist()
+            if len(bbox) not in [4, 5]:
+                raise ValueError(f"bbox has to has length 4 or 5. Got {bbox}.")
+            from_bbox_mode = annotation["bbox_mode"]
+            to_bbox_mode = BoxMode.XYWH_ABS if len(bbox) == 4 else BoxMode.XYWHA_ABS
+            bbox = BoxMode.convert(bbox, from_bbox_mode, to_bbox_mode)
+
+            # COCO requirement: instance area
+            if "segmentation" in annotation:
+                # Computing areas for instances by counting the pixels
+                segmentation = annotation["segmentation"]
+                # TODO: check segmentation type: RLE, BinaryMask or Polygon
+                if isinstance(segmentation, list):
+                    polygons = PolygonMasks([segmentation])
+                    area = polygons.area()[0].item()
+                elif isinstance(segmentation, dict):  # RLE
+                    area = mask_util.area(segmentation).item()
+                else:
+                    raise TypeError(f"Unknown segmentation type {type(segmentation)}!")
+            else:
+                # Computing areas using bounding boxes
+                if to_bbox_mode == BoxMode.XYWH_ABS:
+                    bbox_xy = BoxMode.convert(bbox, to_bbox_mode, BoxMode.XYXY_ABS)
+                    area = Boxes([bbox_xy]).area()[0].item()
+                else:
+                    area = RotatedBoxes([bbox]).area()[0].item()
+
+            if "keypoints" in annotation:
+                keypoints = annotation["keypoints"]  # list[int]
+                for idx, v in enumerate(keypoints):
+                    if idx % 3 != 2:
+                        # COCO's segmentation coordinates are floating points in [0, H or W],
+                        # but keypoint coordinates are integers in [0, H-1 or W-1]
+                        # For COCO format consistency we substract 0.5
+                        # https://github.com/facebookresearch/detectron2/pull/175#issuecomment-551202163
+                        keypoints[idx] = v - 0.5
+                if "num_keypoints" in annotation:
+                    num_keypoints = annotation["num_keypoints"]
+                else:
+                    num_keypoints = sum(kp > 0 for kp in keypoints[2::3])
+
+            # COCO requirement:
+            #   linking annotations to images
+            #   "id" field must start with 1
+            coco_annotation["id"] = len(coco_annotations) + 1
+            coco_annotation["image_id"] = coco_image["id"]
+            coco_annotation["bbox"] = [round(float(x), 3) for x in bbox]
+            coco_annotation["area"] = float(area)
+            coco_annotation["iscrowd"] = int(annotation.get("iscrowd", 0))
+            coco_annotation["category_id"] = int(reverse_id_mapper(annotation["category_id"]))
+
+            # Add optional fields
+            if "keypoints" in annotation:
+                coco_annotation["keypoints"] = keypoints
+                coco_annotation["num_keypoints"] = num_keypoints
+
+            if "segmentation" in annotation:
+                seg = coco_annotation["segmentation"] = annotation["segmentation"]
+                if isinstance(seg, dict):  # RLE
+                    counts = seg["counts"]
+                    if not isinstance(counts, str):
+                        # make it json-serializable
+                        seg["counts"] = counts.decode("ascii")
+
+            coco_annotations.append(coco_annotation)
+
+    logger.info(
+        "Conversion finished, "
+        f"#images: {len(coco_images)}, #annotations: {len(coco_annotations)}"
+    )
+
+    info = {
+        "date_created": str(datetime.datetime.now()),
+        "description": "Automatically generated COCO json file for Detectron2.",
+    }
+    coco_dict = {"info": info, "images": coco_images, "categories": categories, "licenses": None}
+    if len(coco_annotations) > 0:
+        coco_dict["annotations"] = coco_annotations
+    return coco_dict
+
+
+
+
+# File "/workspace/detectron2/detectron2/data/dataset_mapper.py", line 154, in __call__
+#     image = utils.read_image(dataset_dict["file_name"], format=self.image_format)
+#   File "/workspace/detectron2/detectron2/data/detection_utils.py", line 181, in read_image
+#     image = Image.open(f)
+#   File "/usr/local/lib/python3.10/dist-packages/PIL/Image.py", line 3305, in open
+#     raise UnidentifiedImageError(msg)
--- README.md
+++ README.md
@@ -1,15 +1,76 @@
-# kpt_det_detectron
+Implementation of the absolute HPE pipeline in the master thesis of Kerim Turacan.
+This include thes dataset creation and the 2step approach of detecing the 2D pose in an spherical projected LiDAR image and lifting into 3D.
+The CARLA simulator is used for synthetic data generation (see Sec. 1), where AV scences are created within the realistic UE4 simulation environments.
+The most important actors are spawnable and configureable pedestrians and cars. On one car in each scene a (semenatic) LiDAR is attached, from which the dataset used in this work was created.
+The autopilot feature of actors in CARLA make them move autonomously at (mostly) high realsism.
+However, with the used CARLA version 0.9.13 the poses of pedestrians were often very similar with the arms pointing straight downwards. Natural mutual interaction could not be observed, as the goal of the autopilot feature is just to move to the target distination; other interactions are not implemented by default.
+It could also be observed that some persons form T-poses.
+Additional problems has been encoutered in the synchronisation of the CARLA world state data and the measurments of the utilized LiDAR sensor. This resulted in joint locations of pedestrians not being consistent with the sensor's point cloud. When projected to an spherical image, the keypoints were sometimes not part of the target instance. Unfortunately, this problem seemed to be persistent with other sensor types. Later version may solve this issues as this drastically worsens the quality of the ground-truth keypoint annotations. To counter-act, a thresholding was implemented and frames discarded if this problem occured.
+
+After creating the labeled LiDAR dataset, the LiDAR data gets projected onto an image plane using Spherical Projection (see utils/fc_matrix_and_projection_calculation.py). The Spherical Projection Image is dependent on the projection matrix, which defines the granularity of the projection and the employed FOV. Ideally a high FOV can be used first, from which a subimage can be then cropped out, which yields the desired FOV.
+
+The Spherical Projection Image is used as input of a 2D estimator. Here, the Detectron2 keypoint_rcnn_R_50_FPN is used.
+Feature extraction over the input image was performed using a ResNet-50-FPN backbone that yields a set of convolutional feature maps to extract the ROIs. A box head performs object classification and bounding box regression using the feature maps provided by the backbone. An optional mask head yields instance segmentation masks, and a keypoint detection head predicts specific key points on the objects detected by the box head network. This keypoint head is designed to predict the 16 keypoints delineated by the adapted custom CARLA skeleton structure.
+
+The 2D ouputs are then passed to a lfting network, which aims to lift the 2D ouputs of the 2D estimator into 3D. The network consists of 2 regression networks, which predict the scalar root distance r_Root and the relative distance pose r_rel. the 3D pose can be recovered by (1_N * r_Root + r_rel) * UDV.
+UDV are the unit direction vectors including the normalized cartesian componenents of the 2D joints. This is acchieved by converting the 2D pixel locations to spherical coordinates by inverse spherical projection and then the unit direction vectors are computed an concatenated to build UDV.
+
+With a smaller dataset available and faster convergence an better absolute translation prediction, onto the UDV inputs the median depth of the instance segmented point cloud can be appended. the instance segmention information comes from the mask head of the 2D estimator output.
+
+The loss function combines pose errors, distance errors and joint relation constraints.
 
 ## 1. Data Generation
         
-with carla python and running carla simulator client, generate data with open3d_lidar_cuboids_skeleton.py
-file ideally located locally in carla repo pythonapi/examples
+While running the carla simulator server in a docker container (see https://carla.readthedocs.io/en/latest/build_docker/), generate the sensor and metadata. File data_generation/open3d_lidar_cuboids_skeleton.py is ideally located locally in carla repo pythonapi/examples --> two carla repos needed: 1. docker container as the carla-server, 2.repo including the carla python api, from which the data generation script is executed. Make sure the carla versions of the docker container and the local one matches.
 
 Following files are being created:
-PCL (.PLY), calib file with W2S matrix, labels (additional information), 
-raw data (data = np.frombuffer(point_cloud.raw_data, dtype=np.dtype([
+PCL (.PLY) from the xyz-values of rawdata, calib file with W2S matrix, metdadata from the CARLA world state (most importantly the CARLA skeleton for HPE)
+
+raw data consists of (data = np.frombuffer(point_cloud.raw_data, dtype=np.dtype([
         ('x', np.float32), ('y', np.float32), ('z', np.float32),
         ('CosAngle', np.float32), ('ObjIdx', np.uint32), ('ObjTag', np.uint32)])) )
+
+## 2. Custom CARLA skeleton
+CARLA natively supports only its distinct skeleton representation, making it incompatible with direct transformation to popular skeleton representations like Human3.6M or SMPL. This work utilizes selected CARLA joints to construct a custom skeleton structure to avoid the complexities and potential errors associated with converting to another joint representation.
+
+The specific CARLA joint names extracted are:
+BONE_KEYS = [
+        'crl_hips__C',        # Pelvis              # 0
+        'crl_thigh__R',       # R_Hip               # 1
+        'crl_leg__R',         # R_Knee              # 2
+        'crl_foot__R',        # R_Ankle             # 3
+        'crl_thigh__L',       # L_Hip               # 4
+        'crl_leg__L',         # L_Knee              # 5
+        'crl_foot__L',        # L_Ankle             # 6
+        'crl_spine01__C',     # Torso               # 7
+        'crl_neck__C',        # Neck                # 8
+        'crl_Head__C',        # Head                # 9
+        'crl_arm__R',         # R_Shoulder          # 10
+        'crl_foreArm__R',     # R_Elbow             # 11
+        'crl_hand__R',        # R_Wrist             # 12
+        'crl_arm__L',         # L_Shoulder          # 13
+        'crl_foreArm__L',     # L_Elbow             # 14
+        'crl_hand__L'         # L_Wrist             # 15
+]
+
+These joints are used to construct the connected pose represntation. The underlying joint connectons foming the "bones" is defined by:
+KINTREE_TABLE = np.array([ 
+    [0, 1],     # Pelvis -> R_Hip               # 0
+    [1, 2],     # R_Hip -> R_Knee               # 1
+    [2, 3],     # R_Knee -> R_Ankle             # 2
+    [0, 4],     # Pelvis -> L_Hip               # 3
+    [4, 5],     # L_Hip -> L_Knee               # 4
+    [5, 6],     # L_Knee -> L_Ankle             # 5
+    [0, 7],     # Pelvis -> Torso               # 6
+    [7, 8],     # Torso -> Neck                 # 7
+    [8, 9],     # Neck -> Head                  # 8
+    [7, 10],    # Torso -> R_Shoulder           # 9       
+    [10, 11],   # R_Shoulder -> R_Elbow         # 10
+    [11, 12],   # R_Elbow -> R_Wrist            # 11
+    [7, 13],    # Torso -> L_Shoulder           # 12
+    [13, 14],   # L_Shoulder -> L_Elbow         # 13
+    [14, 15]    # L_Elbow -> L_Wrist            # 14
+]).T
 
 ## 2. Create Detectron usable Dataset
 image representation of the lidar scans are created (img) & annototations (labels) used for training in COCO format
